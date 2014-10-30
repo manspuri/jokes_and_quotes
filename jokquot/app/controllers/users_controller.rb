0cc980dadf85512634f6c5be7a914508ab13626a
Mansi Puri <mans.puri@gmail.com>
2014-10-30 16:05:50 -0500

    basic sign-up working

---
+++ jokquot/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ActionController::Base
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+end
--- jokquot/app/models/user.rb
+++ jokquot/app/models/user.rb
@@ -1,5 +1,30 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :posts
   has_many :comments
   has_many :votes
+
+  validates :email, :first_name, :last_name, :password_hash, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password_input)
+    user = User.find_by(email: email)
+
+    if user && user.password == password_input
+      return user
+    end
+  end
 end
