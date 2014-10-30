eb6a09928baa9611764c261534c0c8ec98a82fee
Megan Gaebler <megan.gaebler@gmail.com>
2014-10-30 15:21:39 -0500

    stashing things before merge, will do real commits in a minute

--- jokquot/app/models/vote.rb
+++ jokquot/app/models/vote.rb
@@ -1,4 +1,19 @@
 class Vote < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :voteable, polymorphic: true
+
+  validates :user, presence: true
+
+  def upvote(post_or_comment, post_or_comment_id)
+    voteable_object = post_or_comment.class.find(post_or_comment_id)
+
+    voteable_object.votes << self
+  end
+
+  def downvote(post_or_comment, post_or_comment_id)
+    voteable_object = post_or_comment.class.find(post_or_comment_id)
+
+    
+
+  end
 end
