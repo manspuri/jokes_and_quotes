c9f4b16366a095a0dcaff284f834210a037254a2
Megan Gaebler <megan.gaebler@gmail.com>
2014-10-30 15:51:02 -0500

    Added upvote, downvote, and vote_tally functionality

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,23 @@ class Vote < ActiveRecord::Base
   belongs_to  :voteable, polymorphic: true
 
   validates :user, presence: true
+
+  def upvote
+    self.value = 1
+    self.save
+  end
+
+  def downvote
+    self.value = -1
+    self.save
+  end
+
+  def self.tally_votes_for(voteable_object)
+    votes = Vote.where(voteable_id: voteable_object.id)
+    votes.sum(:value)
+  end
 end
+
+
+
+
