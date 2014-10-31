3b9ed3e8807bf00ae0c8b6e6587d2aae7ab3a4de
Dustin Snyder <roboprog010@gmail.com>
2014-10-31 16:36:30 -0500

    add working get_comments_json with specs - all specs pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,26 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many   :votes, as: :voteable
   has_many   :comments, as: :commentable
+
+  def get_comments_json
+  	build_comments_nest(self)
+  end
+
+  private
+
+  def build_comments_nest(user_submission)
+  	final = user_submission.comments.map do |comment|
+  		build_comment_json(comment)
+  	end
+    final == [] ? {} : final
+  end
+
+  def build_comment_json(comment)
+  	obj = { "text" => comment.text,
+  					"username" => comment.user.username }
+  	if comment.comments.count > 0
+  		obj["comments"] = build_comments_nest(comment)
+  	end
+  	obj
+  end
 end
