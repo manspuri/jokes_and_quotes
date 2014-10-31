d0c2e694621fb879048d3a53bc8263645d5fb3d5
Mansi Puri  & Megan Gaebler <pair+manspuri.missandry@devbootcamp.com>
2014-10-31 10:37:57 -0500

    minor changes to vote migrations

--- db/migrate/20141029221933_create_votes.rb
+++ db/migrate/20141029221933_create_votes.rb
@@ -2,7 +2,6 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.belongs_to :user
-      t.integer    :value, default: 0
       t.integer    :voteable_id
       t.string     :voteable_type
 
