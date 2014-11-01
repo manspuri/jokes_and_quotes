61ef41f1d7ab180be2fb5818c130d81f48f17586
Megan Gaebler <megan.gaebler@gmail.com>
2014-10-31 20:58:16 -0500

    saved vote helper durrR

--- app/helpers/vote_helper.rb
+++ app/helpers/vote_helper.rb
@@ -1,6 +1,26 @@
 module VoteHelper
 
-  def find_vote(votes, post)
+  def self.find_vote(votes, post)
     votes.find_by(voteable_id: post.id, voteable_type: post.stringify_class)
+  end
+
+  def upvote
+    self.value = 1
+    self.save
+    return self
+  end
+
+  def downvote
+    self.value = -1
+    self.save
+    return self
+  end
+
+  def put_or_post
+    if self.value == 0
+      return :post
+    else
+      return :put
+    end
   end
 end
