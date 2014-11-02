4c329d676e53242518c553679218d408d8473f91
Isaac Keith <iceykohnn@gmail.com>
2014-11-01 19:51:12 -0500

    finished the controller with basic structure of the tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,55 @@
 class PostsController < ApplicationController
+    #This is my best attempt at working with this before_action helper and the rails syntax for controllers. Heeeere we go...
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    	 @posts = Post.all
+    @posts = Post.all
        
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+          format.html {redirect_to @post, notice:'Post was successfully created, yo.'}
+          format.json {render :show, status: :created, location: @post}
+      else
+          format.html {render :new}
+          format.json {render json: @post.errors, status: unprocessable_entity}
+      end
     end
+  end
 
-    def create
+  def show
 
-    end
+  end
 
-    def show
+  def update
+      respond_to do |format|
+          if @post.update(post_params)
+              format.html {redirect_to @post, notice: 'Post was successfully updated, yo.'}
+              format.json {render :show, status: :ok, location: @post}
+          else
+              format.html {render :edit}
+              format.json {render json: @post.errors, status: unprocessable_entity}
+          end
+      end
+  end
 
-    end
+  def destroy
+      @post.destroy
+      respond_to do |format|
+          format.html {redirect_to posts_url, notice: 'Post was successfully destroyed, yo.'}
+          format.json {head :no_content}
+      end
+  end
 
-    def update
+private
+  def set_post
+      @post = Post.find(params[:id])
+  end
 
-    end
-
-    def destroy
-
-    end
+  def post_params
+      params.require(:post).permit(:user_id,:post_type, :text)
+  end
 end
--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,7 +1,92 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 describe PostsController do
-	describe "#index" do 
-		it "should show	all posts"
-	end
+  	describe "#index" do 
+  		it "should assign posts" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            get :index
+  		expect(assigns(:posts).length).to eq(1)
+  	end
+  end
+
+  	describe "#index" do 
+  		it "should have a post with a username" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            get :index
+  		expect(assigns(:posts).user.username).to not_be_nil
+  	end
+  end
+
+  	describe "#index" do 
+  		it "should have a post with a created_at date" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            get :index
+  		expect(assigns(:posts).created_at).to not_be_nil
+  	end
+  end
+
+  	describe "#index" do
+  			it "should have a post with some text" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            get :index 
+  		expect(assigns(:posts).text).to not_be_nil
+  	end
+  end
+
+  	describe "#create" do 
+  		it "should save a post" do 
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            post :create
+  		expect(assigns(:posts).)
+  	end
+  end
+
+  	describe "#create" do 
+  		it "should not save a post without any text" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            post :create
+  		expect(assigns(:posts).)
+  	end
+  end
+
+  	describe "#create" do 
+  		it "should redirect to #show when it saves" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            post :create
+  		expect(assigns(:posts))
+  	end
+  end
+
+  	describe "#show" do 
+  		it "should show a single post" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            get :show
+  		expect(assigns(:posts).length).to eq(1)
+  	end
+  end
+
+  	describe "#update" do 
+  		it "should save new user input" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            post :update
+  		expect(assigns(:posts).)
+  	end
+  end
+
+  	describe "#update" do 
+  		it "should change the database to the new user input" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            post :update
+  		expect(assigns(:posts).)
+  	end
+  end
+
+  	describe "#destroy" do 
+  		it "should delete a post" do
+        Post.create(user_id: 1, post_type:"joke", text: "What do you call a man with a rubbertoe? Roberto")
+            delete :delete
+  		expect(assigns(:posts).text).to be_nil
+  	end
+  end
+
 end
