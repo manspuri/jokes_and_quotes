d92037e891a88874125ea8d017e64068c0868578
Mansi Puri  & Megan Gaebler <pair+manspuri.missandry@devbootcamp.com>
2014-11-02 13:40:47 -0600

    added funky logic to view for more awesome vote buttons

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,12 @@ module UserHelper
   def voted?(post)
     self.votes.pluck(:voteable_id).include?(post.id)
   end
+
+  def upvoted?(post)
+    self.votes.find_by(voteable_id: post.id).value == 1
+  end
+
+  def downvoted?(post)
+    self.votes.find_by(voteable_id: post.id).value == -1
+  end
 end
