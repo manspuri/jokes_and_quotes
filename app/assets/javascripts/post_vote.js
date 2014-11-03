f051991fd900fc74d3c01d8bcba361d4e09780ff
Dustin Snyder <roboprog010@gmail.com>
2014-11-02 20:19:26 -0600

    finished

--- app/assets/javascripts/post_vote.js
+++ app/assets/javascripts/post_vote.js
@@ -33,28 +33,39 @@ on click .vote_control
 
 // }
 
-$(document).on('click','.vote_control', function(e){
-	var value = 0;
-	if($(e.target).hasClass('upvote')) {
-		value = 1;
-	} else {
-		value = -1;
+$(document).on('click','.vote_control i', function(e){
+	var current_highlight_class = $(e.target).closest('.vote_control').find('.highlight').first();
+	if(current_highlight_class.length > 0) {
+		current_highlight_class = current_highlight_class.hasClass('upvote') ? 'upvote' : 'downvote';
 	}
-	var id = $(e.target).closest('li').data('id');
-	var clicked = $(e.target);
-	$.ajax({
-		url: '/comments/'+id+'/votes',
-		type: 'post',
-		dataType: 'json',
-		data: 'vote[value]='+value+'&vote[voteable_id]='+id+'&vote[voteable_type]=Comment',
-		success: function(response) {
-			clicked.closest('.vote_control').find('i').removeClass('highlight');
-			clicked.addClass('highlight');
-			var span = clicked.closest('.vote_control').find('span').first();
-			var span_val = span.text() * 1;
-			span.text(span_val + value);
-		}, error: function(response) {
-			console.log(response);
+	var current_class = $(e.target).hasClass('upvote') ? 'upvote' : 'downvote';
+	if(current_class != current_highlight_class) {
+		var value = 0;
+		if($(e.target).hasClass('upvote')) {
+			value = 1;
+		} else {
+			value = -1;
 		}
-	});
+		var id = $(e.target).closest('li').data('id');
+		var clicked = $(e.target);
+		$.ajax({
+			url: '/comments/'+id+'/votes',
+			type: 'post',
+			dataType: 'json',
+			data: 'vote[value]='+value+'&vote[voteable_id]='+id+'&vote[voteable_type]=Comment',
+			success: function(response) {
+				if(clicked.closest('.vote_control').find('.highlight').length) {
+					clicked.closest('.vote_control').find('i').removeClass('highlight');
+				} else {
+					clicked.closest('.vote_control').find('i').removeClass('highlight');
+					clicked.addClass('highlight');
+				}
+				var span = clicked.closest('.vote_control').find('span').first();
+				var span_val = span.text() * 1;
+				span.text(span_val + value);
+			}, error: function(response) {
+				console.log(response);
+			}
+		});
+	}
 });
